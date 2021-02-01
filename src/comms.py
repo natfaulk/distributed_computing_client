71590c04b99ff8b9c68a4787808af9b98147ed71
natfaulk <nfaulk@hotmail.co.uk>
2021-02-02 11:30:26 +1300

    better server address parameters in .env

--- src/comms.py
+++ src/comms.py
@@ -5,6 +5,7 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 SERVER_PORT=os.getenv('SERVER_PORT')
 SERVER_ADDR=os.getenv('SERVER_ADDR')
+SERVER_USE_HTTP_HTTPS=os.getenv('USE_HTTP_HTTPS')
 
 if SERVER_PORT is None:
   SERVER_PORT=3000
@@ -12,12 +13,17 @@ if SERVER_PORT is None:
 if SERVER_ADDR is None:
   SERVER_ADDR='127.0.0.1'
 
+if SERVER_USE_HTTP_HTTPS.capitalize()=='HTTPS':
+  SERVER_URL=f'https://{SERVER_ADDR}/'
+else:
+  SERVER_URL=f'http://{SERVER_ADDR}:{SERVER_PORT}/'
+
 class Comms:
   def __init__(self, _client_id):
     self.id=_client_id
 
   def pingServer(self):
-    r = requests.post(f'http://{SERVER_ADDR}:{SERVER_PORT}/ping', data={'id':self.id})
+    r = requests.post(f'{SERVER_URL}ping', data={'id':self.id})
     print(r.text)
 
   def jobComplete(self, _jobID):
@@ -25,11 +31,11 @@ class Comms:
       'id':self.id,
       'task_id':_jobID
     }
-    r = requests.post(f'http://{SERVER_ADDR}:{SERVER_PORT}/task_complete', data=params)
+    r = requests.post(f'{SERVER_URL}task_complete', data=params)
   
   def getJob(self):
     params={'id':self.id}
-    r = requests.post(f'http://{SERVER_ADDR}:{SERVER_PORT}/get_task', data=params)
+    r = requests.post(f'{SERVER_URL}get_task', data=params)
     return r
 
   def uploadJob(self, _jobID, _filename):
@@ -40,7 +46,7 @@ class Comms:
         'taskOutput': ('filename', open(_filename, 'rb'), 'application/binary')
       })
 
-    r = requests.post(f'http://{SERVER_ADDR}:{SERVER_PORT}/upload_task', data=payload, headers={
+    r = requests.post(f'{SERVER_URL}upload_task', data=payload, headers={
       'Content-Type': payload.content_type
     })
 
